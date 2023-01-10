3871467746aae3781f95bf57b289ef7c114c4980
achumanivannan <achuma1702@gmail.com>
2023-01-10 16:59:58 +0530

    first commit in master

---
+++ src/org/arr/ArrayLi.java
@@ -0,0 +1,34 @@
+package org.arr;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayLi {
+public static void main(String[] args) {
+		
+	Set<Integer> s=new HashSet<Integer>();//10,20,30,90,10,10,40,50
+    // 30,40,50,60,80
+	s.add(10);
+	s.add(20);
+	s.add(30);
+	s.add(90);
+	s.add(10);
+	s.add(10);
+	s.add(40);
+	s.add(50);
+	
+	Set<Integer> a=new HashSet<Integer>();
+	a.add(30);
+	a.add(40);
+	a.add(50);
+	a.add(60);
+	a.add(80);
+	
+	System.out.println(s);
+	System.out.println(a);
+	s.removeAll(a);
+	System.out.println(s);
+
+
+}
+	}
---
+++ src/org/arr/Arrays.java
@@ -0,0 +1,21 @@
+package org.arr;
+
+
+public class Arrays {
+
+
+public static void main(String[] args) {
+	
+
+	int a[]= new int[-5];
+	a[0]=10;
+	a[1]=20;
+	a[2]=30;
+	a[3]=40;
+	
+		 System.out.println(a[3]);
+		
+	
+	
+}
+}
